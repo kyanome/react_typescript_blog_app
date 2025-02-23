5f886b7e3874977a42ff79c15697061c66aff5db
kyanome <yanome.keita@gmail.com>
2025-02-23 17:25:35 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Layout from "@/components/Layout";
 import Home from "@/pages/Home";
 import PostDetail from "@/pages/PostDetail";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />} />
-            <Route path="posts/:id" element={<PostDetail />}></Route>
+            <Route path="posts/:id" element={<PostDetail />} />
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
